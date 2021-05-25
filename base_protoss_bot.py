a4c059db93d2b827cd3ff1136ebc4e9db95c71ab
CyStorm <>
2021-05-25 15:32:27 -0400

    added next step of build order get WG and blink

--- base_protoss_bot.py
+++ base_protoss_bot.py
@@ -1,9 +1,10 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
-from sc2.player import Bot, Computer
+from sc2.player import Bot, Computer, Human
 from sc2.ids.unit_typeid import UnitTypeId as UNITID
 from sc2.ids.ability_id import AbilityId as ABILITYID
 from sc2.ids.buff_id import BuffId as BUFFID
+from sc2.ids.upgrade_id import UpgradeId as UPGRADEID
 
 from loguru import logger
 
@@ -12,7 +13,7 @@ from build_orders import one_gate_expand
 class BaseProtossBot(sc2.BotAI):
     def __init__(self):
         self.build_order_step = 0
-        self.build_order_complete = False
+        self.build_order_stage = 0
         self.build_order = None
 
         self.nexuses = None
@@ -25,6 +26,8 @@ class BaseProtossBot(sc2.BotAI):
 
         self.is_expanding = True
         self.probes_on_gas = 0
+
+        self.has_warpgate = False
         # can do self.gas_buildings built in
 
     def initial_setup(self):
@@ -50,11 +53,11 @@ class BaseProtossBot(sc2.BotAI):
         next_pylon = await self.find_placement(UNITID.COMMANDCENTER, near=self.nexuses.random.position)  # use CC for reasons
         next_expansion = await self.get_next_expansion()
         self.watch_gas_saturation()
-        if(self.build_order_complete):
+        if(self.build_order_stage == 2):
+            print("stage 2")
             self.set_unit_groups()
             # can abosrb building placement into 1 place
-
-            # self.build_pylon(location=next_pylon)
+            self.build_pylon(location=next_pylon)
             # self.expand(location=next_expansion)
             # self.do_chronoboost(self.nexuses.first)
             # self.build_any_structure(UNITID.GATEWAY, location=next_gateway)
@@ -63,15 +66,23 @@ class BaseProtossBot(sc2.BotAI):
             # self.build_pylon(next_pylon)
             # self.build_gas()
             # self.assign_to_gas(3)
-        else:
+        elif (self.build_order_stage == 0):
+            print("stage 0")
             self.set_unit_groups()
             self.do_build_order(natural_location=next_expansion, be=next_pylon, building_location=next_gateway)
-
-    async def distribute_workers(self):
-        print("overwrite")
+        elif (self.build_order_stage == 1):
+            self.set_unit_groups()
+            print("stage 1")
+            self.build_pylon(location=next_pylon)
+            if (self.workers.amount < 45):
+                self.build_worker()
+            if (not self.structures(UNITID.TWILIGHTCOUNCIL)):
+                self.build_any_structure(UNITID.TWILIGHTCOUNCIL, next_gateway)
+            self.train_unit(UNITID.STALKER)
 
     def set_unit_groups(self):
         self.nexuses = self.structures(UNITID.NEXUS)
+        self.gateways = self.structures(UNITID.GATEWAY)
 
     def do_build_order(self, **kwargs):
         '''Do a pre defined build order, should prob re write
@@ -80,7 +91,7 @@ class BaseProtossBot(sc2.BotAI):
         '''
         max_steps = len(self.build_order)
         if (self.build_order_step == max_steps):
-            self.build_order_complete = True
+            self.build_order_stage = 1
             print("build order done")
             return
 
@@ -130,9 +141,9 @@ class BaseProtossBot(sc2.BotAI):
 
     def build_worker(self):
         success = False
-        nexus = self.structures(UNITID.NEXUS).ready.random
-        if (self.can_afford(UNITID.PROBE)):
-            nexus.train(UNITID.PROBE, queue=True)
+        nexus = self.townhalls.idle
+        if (nexus and self.can_afford(UNITID.PROBE)):
+            nexus.random.train(UNITID.PROBE, queue=True)
             success = True
         return success
 
@@ -189,12 +200,14 @@ class BaseProtossBot(sc2.BotAI):
     def remove_from_gas(self, amount, worker=None):
         if (amount):
             workers = self.units(UNITID.PROBE).filter(lambda worker: worker.is_carrying_vespene).random_group_of(amount)
-            for worker in workers:
-                worker.gather(self.owned_minearls.closest_to(worker.position), queue=True)
+            for probe in workers:
+                probe.gather(self.owned_minearls.closest_to(probe.position), queue=True)
         if (worker):
             worker.gather(self.owned_minearls.closest_to(worker.position), queue=True)
 
     def watch_gas_saturation(self):
+        '''Assign probes to gas as the internal variable dictates
+        '''
         count = 0
         for assimilator in self.gas_buildings:
             count += assimilator.assigned_harvesters
@@ -205,7 +218,15 @@ class BaseProtossBot(sc2.BotAI):
         elif(count > self.probes_on_gas):
             self.remove_from_gas(count - self.probes_on_gas)
 
+    def watch_mineral_saturation(self):
+        '''Optimize mineral saturaton, from oversaturation in bases, or non-optimal per patch saturation
+        '''
+        for nexus in self.nexuses.ready:
+            pass
+
     def do_chronoboost(self, target_structure: sc2.unit.Unit):
+        '''Uses chronoboost on target structure
+        '''
         success = False
         if (not target_structure.has_buff(BUFFID.CHRONOBOOSTENERGYCOST)):
             source = None
@@ -227,6 +248,17 @@ class BaseProtossBot(sc2.BotAI):
                 success = True
         return success
 
+    def train_unit(self, unit_id: UNITID, amount=1):
+
+        success = False
+        if (self.tech_requirement_progress(unit_id) < 1):
+            return success
+        gateway = self.gateways.idle
+        if (gateway and self.can_afford(unit_id) and self.can_feed(unit_id)):
+            gateway.random.train(unit_id)
+            success = True
+        return success
+
     async def on_unit_created(self, unit: sc2.unit.Unit):
         '''built in function
         '''
@@ -241,6 +273,14 @@ class BaseProtossBot(sc2.BotAI):
             self.owned_minearls += new_minearls
             new_gases = self.vespene_geyser.closer_than(distance=8, position=unit.position)
             self.owned_empty_geysers += new_gases
+        elif (unit.type_id == UNITID.CYBERNETICSCORE):
+            success = False
+            while (not success):
+                success = self.research(UPGRADEID.WARPGATERESEARCH)
+        elif (unit.type_id == UNITID.TWILIGHTCOUNCIL):
+            success = False
+            while (not success):
+                success = self.research(UPGRADEID.BLINKTECH)
         print("done building {}".format(unit))
 
     async def on_unit_destroyed(self, unit_tag: int):
@@ -253,6 +293,7 @@ class BaseProtossBot(sc2.BotAI):
 
 def main():
     run_game(maps.get("AscensiontoAiurLE"), [Bot(Race.Protoss, BaseProtossBot()), Computer(Race.Terran, Difficulty.Easy)], realtime=True)
+    # run_game(maps.get("AscensiontoAiurLE"), [Human(Race.Terran), Bot(Race.Protoss, BaseProtossBot())], realtime=True)
 
 if __name__ == "__main__":
     main()
